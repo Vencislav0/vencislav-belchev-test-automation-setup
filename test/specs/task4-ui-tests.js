d6e915f599597abc40845c1c3655881cecb8f994
Vencislav0 <darkslayer629@gmail.com>
2025-01-27 14:40:21 +0000

    Task 4 adressed and fixed issues

--- test/specs/task4-ui-tests.js
+++ test/specs/task4-ui-tests.js
@@ -2,29 +2,29 @@ const { assert } = require('chai')
 
 describe('Add/Remove Elements e2e', () => {
   it('should add and remove elements when clicking the buttons', async () => {
-    browser.url('https://the-internet.herokuapp.com/add_remove_elements/')
+    await browser.url('https://the-internet.herokuapp.com/add_remove_elements/')
 
     const addElementButton = await $('//button[text()="Add Element"]')
-    let deleteButton = await $('//button[text()="Delete"]')
+    const deleteButton = await $('//button[text()="Delete"]')
 
     assert.isTrue(await addElementButton.isDisplayed(), 'Add button should be displayed')
     assert.isFalse(await deleteButton.isDisplayed(), 'Delete button should not be displayed until the add button is clicked')
     assert.equal(await $('h3').getText(), 'Add/Remove Elements', 'the header should display Add/Remove Elements')
 
-    for (i = 0; i < 10; i++) {
+    for (let i = 0; i < 10; i++) {
       await addElementButton.click()
     }
 
-    deleteButton = await $$('//button[text()="Delete"]')
+    let deleteButtonArray = await $$('//button[text()="Delete"]')
 
-    assert.equal(deleteButton.length, 10, 'Delete buttons should be 10 after clicking add element 10 times')
+    assert.equal(deleteButtonArray.length, 10, 'Delete buttons should be 10 after clicking add element 10 times')
 
-    for (i = 10; i > 0; i--) {
-      const lastDeleteButton = deleteButton[deleteButton.length - 1]
+    for (let i = 10; i > 0; i--) {
+      const lastDeleteButton = deleteButtonArray[deleteButtonArray.length - 1]
       await lastDeleteButton.click()
 
-      deleteButton = await $$('//button[text()="Delete"]')
-      assert.equal(deleteButton.length, i - 1, 'Delete button should be removed when clicked')
+      deleteButtonArray = await $$('//button[text()="Delete"]')
+      assert.equal(deleteButtonArray.length, i - 1, 'Delete button should be removed when clicked')
     }
 
     assert.isFalse(await $('//button[text()="Delete"]').isDisplayed(), 'No delete buttons should be left after clicking all of them')
@@ -33,7 +33,7 @@ describe('Add/Remove Elements e2e', () => {
 
 describe('JavaScript Alerts e2e', () => {
   it('Confirming functionality of all alerts displayed', async () => {
-    browser.url('https://the-internet.herokuapp.com/javascript_alerts')
+    await browser.url('https://the-internet.herokuapp.com/javascript_alerts')
 
     const clickForAlert = await $('//button[text()="Click for JS Alert"]')
     const clickForAlertConfirm = await $('//button[text()="Click for JS Confirm"]')
@@ -50,7 +50,6 @@ describe('JavaScript Alerts e2e', () => {
     assert.isTrue(await clickForAlertPrompt.isDisplayed(), 'Click for alert prompt button should be visible')
     assert.equal(await $('h3').getText(), 'JavaScript Alerts', 'Header should be displayed with text JavaScript Alerts')
 
-    await browser.pause(2000)
     //First alert accept case
     await clickForAlert.click()
     assert.equal(await browser.getAlertText(), 'I am a JS Alert', 'alert text should equal "I am a JS Alert"')
@@ -70,10 +69,17 @@ describe('JavaScript Alerts e2e', () => {
     assert.equal(await $('//p[@id="result"]').getText(), 'You clicked: Cancel', '"You clicked: Cancel" should be displayed after accepting the alert')
 
     //Third alert prompt case
-    await clickForAlertPrompt.click()
-    assert.equal(await browser.getAlertText(), 'I am a JS prompt', 'alert text should equal "I am a JS prompt"')
-    await browser.sendAlertText('test')
-    await browser.acceptAlert()
+
+    await browser.waitUntil(
+      async () => {
+        await clickForAlertPrompt.click()
+        assert.equal(await browser.getAlertText(), 'I am a JS prompt', 'alert text should equal "I am a JS prompt"')
+        await browser.sendAlertText('test')
+        await browser.acceptAlert()
+        return (await $('//p[@id="result"]').getText()) == 'You entered: test'
+      },
+      { timeout: 5000, interval: 500, timeoutMsg: 'Failed to input text in alert field' },
+    )
 
     assert.equal(await $('//p[@id="result"]').getText(), 'You entered: test', '"You entered: test" should be displayed after accepting the alert')
   })
@@ -81,7 +87,7 @@ describe('JavaScript Alerts e2e', () => {
 
 describe('Checkboxes e2e', () => {
   it('Confirming functionality of all checkboxes displayed', async () => {
-    browser.url('https://the-internet.herokuapp.com/checkboxes')
+    await browser.url('https://the-internet.herokuapp.com/checkboxes')
 
     const checkboxes = await $$('//input[@type="checkbox"]')
 
@@ -106,7 +112,7 @@ describe('Checkboxes e2e', () => {
 
 describe('Hovers e2e', () => {
   it('Confirming functionality of Hovers', async () => {
-    browser.url('https://the-internet.herokuapp.com/hovers')
+    await browser.url('https://the-internet.herokuapp.com/hovers')
 
     const images = await $$('//div[@class="figure"]')
     const names = await $$('//h5')
@@ -124,14 +130,17 @@ describe('Hovers e2e', () => {
 
     await images[0].moveTo()
     assert.isTrue(await names[0].isDisplayed(), 'First name should be displayed when the first image is hovered on')
+    assert.include(await names[0].getText(), 'user1', 'First name of the hovered image should be correct')
     assert.isTrue(await viewProfileButton[0].isDisplayed(), 'First "View Profile" button should be displayed when the first image is hovered on')
 
     await images[1].moveTo()
     assert.isTrue(await names[1].isDisplayed(), 'Second name should be displayed when the first image is hovered on')
+    assert.include(await names[1].getText(), 'user2', 'Second name of the hovered image should be correct')
     assert.isTrue(await viewProfileButton[1].isDisplayed(), 'Second "View Profile" button should be displayed when the first image is hovered on')
 
     await images[2].moveTo()
     assert.isTrue(await names[2].isDisplayed(), 'Third name should be displayed when the first image is hovered on')
+    assert.include(await names[2].getText(), 'user3', 'Third name of the hovered image should be correct')
     assert.isTrue(await viewProfileButton[2].isDisplayed(), 'Third "View Profile" button should be displayed when the first image is hovered on')
 
     await viewProfileButton[2].click()
@@ -141,12 +150,13 @@ describe('Hovers e2e', () => {
       'should be redirected to https://the-internet.herokuapp.com/users/3 after clicking the view profile button',
     )
     assert.isTrue(await $('h1').isDisplayed(), 'the header Not Found should be displayed')
+    assert.equal(await $('h1').getText(), 'Not Found', 'the header text should be "Not Found"')
   })
 })
 
 describe('Opening a new window', () => {
   it('Should open a new window when clicking the button', async () => {
-    browser.url('https://the-internet.herokuapp.com/windows')
+    await browser.url('https://the-internet.herokuapp.com/windows')
 
     const clickHereLink = await $('//a[text()="Click Here"]')
     const initialWindows = await browser.getWindowHandles()
@@ -155,12 +165,12 @@ describe('Opening a new window', () => {
     assert.equal(await $('h3').getText(), 'Opening a new window', 'Header with "Opening a new window" text should be displayed')
 
     await clickHereLink.click()
-    const Windows = await browser.getWindowHandles()
-    const WindowCount = Windows.length
+    const windows = await browser.getWindowHandles()
+    const windowCount = windows.length
 
-    assert.equal(WindowCount, initialWindowCount + 1, 'A new tab should be opened')
+    assert.equal(windowCount, initialWindowCount + 1, 'A new tab should be opened')
 
-    const newTab = Windows.find((handle) => !initialWindows.includes(handle))
+    const newTab = windows.find((handle) => !initialWindows.includes(handle))
 
     await browser.switchToWindow(newTab)
 
