2990a1f992561043cc02fa80b7bae2569470331f
Vencislav0 <darkslayer629@gmail.com>
2025-03-05 00:41:49 +0000

    Modified wdio config file

--- wdio.conf.js
+++ wdio.conf.js
@@ -25,7 +25,7 @@ exports.config = {
   // The path of the spec files will be resolved relative from the directory of
   // of the config file unless it's absolute.
   //
-  specs: [['./test/specs/eMag-Tests-e2e/testcase6-social-media-links.js']],
+  specs: [['./test/specs/eMag-Tests-e2e/*.js']],
   // Patterns to exclude.
   exclude: [
     // 'path/to/excluded/files'
